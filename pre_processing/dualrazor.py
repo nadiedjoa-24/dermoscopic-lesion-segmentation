f262549a3f0fcc52be489d886e07c96937a7e6e6
Agshay Nadanakumar <agshay.nadanakumar@telecom-paris.fr>
2025-11-01 13:15:48 +0100

    Debug étape 1 Dual Razor

--- pre_processing/dualrazor.py
+++ pre_processing/dualrazor.py
@@ -14,9 +14,15 @@ im2 = cv2.cvtColor(im1, cv2.COLOR_BGR2RGB)
 
 Ma = np.array([0,1,1,1,1,1,1,1,1,1,1,1,0], dtype=np.uint8)
 Mb = np.array([[0,0,0,0,0,0,0,0,0],[0,1,0,0,0,0,0,0,0],[0,0,1,0,0,0,0,0,0],[0,0,0,1,0,0,0,0,0],[0,0,0,0,1,0,0,0,0],[0,0,0,0,0,1,0,0,0],[0,0,0,0,0,0,1,0,0],[0,0,0,0,0,0,0,1,0],[0,0,0,0,0,0,0,0,1]], dtype = np.uint8)
+
+
+Md = np.zeros((9, 9), dtype=np.uint8)
+for i in range(1, 8):  # lignes de 1 à 7
+    Md[i, i] = 1   
+
 Mc = np.array([[0],[1],[1],[1],[1],[1],[1],[1],[1],[1],[1],[1],[0]], dtype = np.uint8)
 
-M = [Ma, Mb, Mc]
+M = [Ma, Md, Mc]
 
 #j'extraie les canaux R,G,B de mon image
 
@@ -27,16 +33,47 @@ B = im2[:,:,2]
 def compute_mask(canal, struct, thresh):
     L = [cv2.morphologyEx(canal, cv2.MORPH_CLOSE, x) for x in struct]
     maxL = np.maximum.reduce(L)
-    G = cv2.subtract(canal, maxL)
+    G = abs(canal - maxL)
     M = np.where(G > thresh, 255, 0).astype(np.uint8) #np.where : renvoie un tableau de la meme taille que G, avec comme element 255 si la condition est vérifié, 0 sinon
     return M
 
-Mr = compute_mask(R, M, 10)
-Mg = compute_mask(G, M, 10)
-Mb = compute_mask(B, M, 10)
+Mr = compute_mask(R, M, 250)
+Mg = compute_mask(G, M, 250)
+Mb = compute_mask(B, M, 250)
+
 
 Mfinal = cv2.merge([Mr, Mg, Mb])
 
-plt.imshow(cv2.cvtColor(Mfinal, cv2.COLOR_BGR2RGB))
+
+test = cv2.morphologyEx(R, cv2.MORPH_CLOSE, Ma)
+test1 = cv2.morphologyEx(R, cv2.MORPH_CLOSE, Md)
+test3 = cv2.morphologyEx(R, cv2.MORPH_CLOSE, Mc)
+maxL = np.maximum.reduce([test, test1, test3])
+G = abs(R - maxL)
+
+'''
+fig, axes = plt.subplots(1, 3, figsize=(12, 4))
+axes[0].imshow(test, cmap='gray', vmin=0, vmax=255)
+axes[0].set_title('test (R, Ma)')
+axes[0].axis('off')
+
+axes[1].imshow(test1, cmap='gray', vmin=0, vmax=255)
+axes[1].set_title('test1 (B, Mb)')
+axes[1].axis('off')
+
+axes[2].imshow(test3, cmap='gray', vmin=0, vmax=255)
+axes[2].set_title('test3 (G, Mc)')
+axes[2].axis('off')
+
+plt.tight_layout()
+plt.show()
+'''
+
+# afficher Mfinal
+plt.figure(figsize=(8, 8))
+plt.imshow(Mfinal)
+plt.title('Mfinal')
+plt.axis('off')
+plt.tight_layout()
 plt.show()
 
